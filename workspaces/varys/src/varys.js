874401d9a37235355a919d0728ef4df7f2e4e88b
Jeroen Knoops <jeroen.knoops@gmail.com>
2020-02-07 21:25:48 +0100

    add show-repository start

--- workspaces/varys/src/varys.js
+++ workspaces/varys/src/varys.js
@@ -2,41 +2,28 @@
 
 const program = require('commander')
 const chalk = require('chalk')
-const fs = require('fs-extra')
 
 const { showRepositories } = require('./commands/show-repositories')
 
 const defaultConfigFileName = 'organizations.json'
 
 const {
-  setVerbose,
-  infoMessage,
-  errorMessage
+  infoMessage
 } = require('./logger/logger')
 
 program
   .version('0.0.1', '-v, --version')
-  //.option('-c, --config <file>', 'set config path. defaults to organizations.json');
+  // .option('-c, --config <file>', 'set config path. defaults to organizations.json');
 
-
-const config = require(`../${defaultConfigFileName}`);
+const config = require(`../${defaultConfigFileName}`)
 
 program
   .command('show-repositories')
   .alias('sr')
   .description('Lord Varys knows everything....about repositories')
-  .action(function(env) {
-    infoMessage(chalk`show repositories`);
-    showRepositories(config);
-  });
+  .action(function (env) {
+    infoMessage(chalk`show repositories`)
+    showRepositories(config)
+  })
 
 program.parse(process.argv)
-
-const { input, output, verbose } = program
-
-const areCliInputParametersValid = ({ input, output }) => {
-  if (!input) {
-    errorMessage(chalk`{red Mandatory input parameter is missing} (run 'extract --help' for usage).`)
-    return false
-  }
-}
