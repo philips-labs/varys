84954792519cf6ebc1e3d31ed77d04823f6fd08a
Marco Franssen <marco.franssen@philips.com>
2020-03-31 15:49:41 +0200

    Move loop logic to lib for better reuse

--- workspaces/varys/lib/commands/add-user-graphql.js
+++ workspaces/varys/lib/commands/add-user-graphql.js
@@ -1,37 +1,41 @@
-const chalk = require('chalk')
-const { graphql } = require('@octokit/graphql')
-const inquirer = require('inquirer')
-const { Octokit } = require('@octokit/rest')
-const Slack = require('slack')
+const chalk = require("chalk");
+const { graphql } = require("@octokit/graphql");
+const inquirer = require("inquirer");
+const { Octokit } = require("@octokit/rest");
+const Slack = require("slack");
 
-const {
-  infoMessage,
-  errorMessage
-} = require('../logger/logger')
+const { infoMessage, errorMessage } = require("../logger/logger");
 
-let token
-let slackToken
+let token;
+let slackToken;
 
 const checkOrganization = ({ organizations }, organization) => {
   for (const ourOrganization of organizations) {
     if (ourOrganization.name === organization) {
-      infoMessage(chalk`Organization (${chalk.green(organization)}) is part of our organizations!`)
-      return
+      infoMessage(
+        chalk`Organization (${chalk.green(
+          organization
+        )}) is part of our organizations!`
+      );
+      return;
     }
   }
-  errorMessage(chalk`Organization (${chalk.green(organization)}) is not part of our organizations`)
-  process.exit(1)
-}
+  errorMessage(
+    chalk`Organization (${chalk.green(
+      organization
+    )}) is not part of our organizations`
+  );
+  process.exit(1);
+};
 
 const fetchUser = async (name, organization) => {
   const graphqlWithAuth = graphql.defaults({
     headers: {
       authorization: `token ${token}`
     }
-  })
+  });
 
-  const query =
-    `
+  const query = `
       query userOrganization($user: String!, $organization: String!) {
         user(login:$user) {
           id
@@ -42,121 +46,164 @@ const fetchUser = async (name, organization) => {
           }
         }
       }
-    `
+    `;
 
   try {
-    return await graphqlWithAuth(query, { user: name, organization: organization })
+    return await graphqlWithAuth(query, {
+      user: name,
+      organization: organization
+    });
   } catch (error) {
-    console.log(error.message)
-    errorMessage(chalk`User is not known in GitHub`)
-    process.exit(1)
+    console.log(error.message);
+    errorMessage(chalk`User is not known in GitHub`);
+    process.exit(1);
   }
-}
+};
 
 const checkUser = async (user, organization) => {
-  const organizationUser = await fetchUser(user, organization)
-  const { name, email, id } = organizationUser.user
+  const organizationUser = await fetchUser(user, organization);
+  const { name, email, id } = organizationUser.user;
 
   if (organizationUser.user.organization) {
-    errorMessage(chalk`User (${chalk.yellow(user)} / ${chalk.yellow(email)} / ${chalk.yellow(name)} / ${chalk.yellow(id)}) is already part of Organization (${chalk.green(organization)})`)
-    process.exit(1)
+    errorMessage(
+      chalk`User (${chalk.yellow(user)} / ${chalk.yellow(
+        email
+      )} / ${chalk.yellow(name)} / ${chalk.yellow(
+        id
+      )}) is already part of Organization (${chalk.green(organization)})`
+    );
+    process.exit(1);
   }
-  infoMessage(chalk`User (${chalk.yellow(user)} / ${chalk.yellow(email)} / ${chalk.yellow(name)} / ${chalk.yellow(id)}) is known and NOT yet member of Organization (${chalk.green(organization)}).`)
-}
+  infoMessage(
+    chalk`User (${chalk.yellow(user)} / ${chalk.yellow(email)} / ${chalk.yellow(
+      name
+    )} / ${chalk.yellow(
+      id
+    )}) is known and NOT yet member of Organization (${chalk.green(
+      organization
+    )}).`
+  );
+};
 
 const checkAlreadyInvited = async (username, organization) => {
   const octokit = new Octokit({
     auth: token
-  })
+  });
 
   const { data } = await octokit.orgs.listPendingInvitations({
     org: organization
-  })
-  infoMessage(chalk`${chalk.yellow(data.length)} users are invited to ${chalk.green(organization)}`)
+  });
+  infoMessage(
+    chalk`${chalk.yellow(data.length)} users are invited to ${chalk.green(
+      organization
+    )}`
+  );
 
   if (data.length > 0) {
-    const invitedMember = data.filter(invitedMember => invitedMember.login === username)
+    const invitedMember = data.filter(
+      invitedMember => invitedMember.login === username
+    );
     if (invitedMember.length > 0) {
-      errorMessage(chalk`${chalk.yellow(username)} is already invited to ${chalk.green(organization)} by ${chalk.yellow(invitedMember[0].inviter.login)} on ${chalk.yellow(invitedMember[0].created_at)}`)
-      process.exit(1)
+      errorMessage(
+        chalk`${chalk.yellow(username)} is already invited to ${chalk.green(
+          organization
+        )} by ${chalk.yellow(invitedMember[0].inviter.login)} on ${chalk.yellow(
+          invitedMember[0].created_at
+        )}`
+      );
+      process.exit(1);
     } else {
-      infoMessage(chalk`${chalk.green(username)} is not yet invited to ${chalk.green(organization)}`)
+      infoMessage(
+        chalk`${chalk.green(username)} is not yet invited to ${chalk.green(
+          organization
+        )}`
+      );
     }
   }
-}
+};
 
 const checkBlocked = async (username, organization) => {
-  infoMessage(chalk`checkBlocked not implemented yet`)
-  return 
+  infoMessage(chalk`checkBlocked not implemented yet`);
+  return;
   // const { data } = await octokit.orgs.checkBlockedUser({
   //  org: organization,
   //  username: username
   //});
   //console.log(data)
-}
-
+};
 
 const checkTeam = (team, organization) => {
-  infoMessage(chalk`Team not implemented yet`)
-}
+  infoMessage(chalk`Team not implemented yet`);
+};
 
 const confirmAdd = () => {
   return {
-    message: 'Do you want to proceed?',
-    name: 'proceed',
-    type: 'confirm',
+    message: "Do you want to proceed?",
+    name: "proceed",
+    type: "confirm",
     default: false
-  }
-}
+  };
+};
 
 const verify = async () => {
-  const getConfirm = await inquirer.prompt(confirmAdd())
+  const getConfirm = await inquirer.prompt(confirmAdd());
   if (getConfirm.proceed) {
-    infoMessage(chalk`Confirmed`)
+    infoMessage(chalk`Confirmed`);
   } else {
-    errorMessage(chalk`User cancelled operation.`)
-    process.exit(1)
+    errorMessage(chalk`User cancelled operation.`);
+    process.exit(1);
   }
-}
+};
 
 const processAction = async (username, organization) => {
-  infoMessage(chalk`Add ${chalk.yellow(username)} to ${chalk.green(organization)}`)
+  infoMessage(
+    chalk`Add ${chalk.yellow(username)} to ${chalk.green(organization)}`
+  );
   const octokit = new Octokit({
     auth: token
-  })
+  });
 
   const { data } = await octokit.orgs.addOrUpdateMembership({
     org: organization,
     username: username,
-    role: 'member'
-  })
-  infoMessage(chalk`Added ${chalk.yellow(username)} to ${chalk.green(organization)}`)
+    role: "member"
+  });
+  infoMessage(
+    chalk`Added ${chalk.yellow(username)} to ${chalk.green(organization)}`
+  );
 
-  const bot = new Slack(slackToken)
+  const bot = new Slack(slackToken);
   var result = await bot.chat.postMessage({
-    channel: '#royal-philips', 
+    channel: "#royal-philips",
     text: `Added *${username}* to *${organization}*`,
-    token: slackToken}
-  )
-  console.log(data)
-}
+    token: slackToken
+  });
+  console.log(data);
+};
 
-const addUser = async (config, { organization, user, team }) => {
-  token = config.githubToken
-  slackToken = config.slackToken
+const addUsers = async (config, { organization, users, team }) => {
+  token = config.githubToken;
+  slackToken = config.slackToken;
 
-  team = team || ''
-  infoMessage(chalk`Add user for: ${chalk.green(organization)} / ${chalk.yellow(user)} / ${chalk.yellow(team)}`)
+  team = team || "";
 
-  checkOrganization(config, organization)
-  await checkUser(user, organization)
-  await checkAlreadyInvited(user, organization)
-  await checkBlocked(user, organization)
-  checkTeam(team, organization)
-  await verify()
-  await processAction(user, organization)
-}
+  users.forEach(async user => {
+    infoMessage(
+      chalk`Add user for: ${chalk.green(organization)} / ${chalk.yellow(
+        user
+      )} / ${chalk.yellow(team)}`
+    );
+
+    checkOrganization(config, organization);
+    await checkUser(user, organization);
+    await checkAlreadyInvited(user, organization);
+    await checkBlocked(user, organization);
+    checkTeam(team, organization);
+    await verify();
+    await processAction(user, organization);
+  });
+};
 
 module.exports = {
-  addUser
-}
+  addUsers
+};
