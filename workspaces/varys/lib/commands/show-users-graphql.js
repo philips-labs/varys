4ad0c46b218c91c7456f6ee224f80fb302372ce9
Marco Franssen <marco.franssen@philips.com>
2020-04-17 10:44:42 +0200

    Refactor code1 command to list command for extendability

--- workspaces/varys/lib/commands/show-users-graphql.js
+++ workspaces/varys/lib/commands/show-users-graphql.js
@@ -4,21 +4,32 @@ import columnify from 'columnify'
 import { infoMessage } from '../logger'
 import { withAuth } from '../graphql'
 
+const queryByOwner = (select) => `
+  query organizationRepositories($owner: String!) {
+    organization(login:$owner) {
+      ${select}
+    }
+  }`
+
 const fetchUsers = async ({ name, token }) => {
   const graphqlWithAuth = withAuth(token)
 
-  const query = `
-      query organizationRepositories($owner: String!) {
-        organization(login:$owner) {
-          membersWithRole {
-            totalCount
-          }
-          pendingMembers {
-            totalCount
+  const query = queryByOwner(`
+    samlIdentityProvider {
+      ssoUrl
+      externalIdentities(first: 100) {
+        edges {
+          node {
+            samlIdentity {
+              nameId
+            }
+            user {
+              login
+            }
           }
         }
       }
-    `
+    }`)
 
   try {
     return await graphqlWithAuth(query, { owner: name })
@@ -28,8 +39,27 @@ const fetchUsers = async ({ name, token }) => {
   }
 }
 
-const display = organizations => {
-  const data = organizations.map(org => {
+const fetchSummary = async ({ name, token }) => {
+  const graphqlWithAuth = withAuth(token)
+
+  const query = queryByOwner(`
+    membersWithRole {
+      totalCount
+    }
+    pendingMembers {
+      totalCount
+    }`)
+
+  try {
+    return await graphqlWithAuth(query, { owner: name })
+  } catch (error) {
+    console.log('Request failed:', error.request)
+    console.log(error.message)
+  }
+}
+
+const display = (organizations) => {
+  const data = organizations.map((org) => {
     const { pendingMembers, membersWithRole } = org.users.organization
     return {
       organisation: org.organizationName,
@@ -48,7 +78,44 @@ const showUsers = async (config, filterOrgs) => {
   const organizations = []
   const fetchOrgs =
     filterOrgs.length > 0
-      ? filterOrgs.map(org => ({ name: org }))
+      ? filterOrgs.map((org) => ({ name: org }))
+      : config.organizations
+
+  for (const organization of fetchOrgs) {
+    let organizationUsers = []
+    infoMessage(chalk`{blue organization: } ${organization.name}`)
+    organizationUsers = await fetchSummary({
+      ...organization,
+      token: config.githubToken
+    })
+    organizations.push({
+      organizationName: organization.name,
+      users: organizationUsers
+    })
+  }
+  display(organizations)
+}
+
+const displayList = (organizations) => {
+  const data = organizations.flatMap(
+    (org) =>
+      org.users.organization.samlIdentityProvider &&
+      org.users.organization.samlIdentityProvider.externalIdentities.edges.map(
+        (edge) => ({
+          organisation: org.organizationName,
+          userId: edge.node.user && edge.node.user.login,
+          code1: edge.node.samlIdentity.nameId
+        })
+      )
+  )
+  console.log(columnify(data.filter(Boolean)))
+}
+
+const listUsers = async (config, filterOrgs) => {
+  const organizations = []
+  const fetchOrgs =
+    filterOrgs.length > 0
+      ? filterOrgs.map((org) => ({ name: org }))
       : config.organizations
 
   for (const organization of fetchOrgs) {
@@ -63,7 +130,7 @@ const showUsers = async (config, filterOrgs) => {
       users: organizationUsers
     })
   }
-  display(organizations)
+  displayList(organizations)
 }
 
-export { showUsers }
+export { showUsers, listUsers }
