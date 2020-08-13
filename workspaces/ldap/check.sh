58c723a91acc9a6d2246c50d7701ac24f0c0a0f2
Jeroen Knoops <jeroen.knoops@philips.com>
2020-08-13 06:48:25 +0200

    add department info

--- workspaces/ldap/check.sh
+++ workspaces/ldap/check.sh
@@ -6,12 +6,32 @@ PASSWORD=${LDAP_PASSWORD}
 BIND_DN=${LDAP_BIND_DN}
 OUTPUT_DIR="report"
 
+showMail() {
+  echo "$ldapresult" | grep -o 'mail:[^,]\+' | grep -o '[^:]\+$'
+}
+
+showAccount() {
+  echo "$ldapresult" | grep -o 'sAMAccountName:[^,]\+' | grep -o '[^:]\+$'
+}
+
+showDepartment() {
+  echo "$ldapresult" | grep -o 'department:[^,]\+' | grep -o '[^:]\+$'
+}
+
+showExtensionAttribute15() {
+  echo "$ldapresult" | grep -o 'extensionAttribute15:[^,]\+' | grep -o '[^:]\+$'
+}
+
+
 checkUser() {
   FILTER="(sAMAccountName=$1)"
 
-  ldapsearch -x -D ${BIND_DN} -w ${PASSWORD}  -H ${URI} -b ${BASE} ${FILTER} -LLL sAMAccountName | grep "sAMAccountName:" > /dev/null
+  ldapresult=$(ldapsearch -x -D ${BIND_DN} -w ${PASSWORD}  -H ${URI} -b ${BASE} ${FILTER} -LLL sAMAccountName extensionAttribute15 mail department)
+  echo "$ldapresult" | grep 'sAMAccountName:' > /dev/null
   if [ $? -eq 0 ]; then
     echo $1 >> ${OUTPUT_DIR}/users-valid.txt
+    #echo "$ldapresult" | grep -o 'mail:[^,]\+' | grep -o '[^:]\+$' >> ${OUTPUT_DIR}/users-valid-ldap-results.txt
+    echo "$(showAccount),$(showMail),$(showExtensionAttribute15),$(showDepartment)" >> ${OUTPUT_DIR}/users-valid-ldap-results.txt
   else
     echo User $1 NOT found.
     echo $1 >> ${OUTPUT_DIR}/user-no-valid.txt
@@ -22,7 +42,7 @@ checkUserFile() {
   while IFS=" " read -r USERID CODE1
   do
     checkUser $CODE1
-  done < <(tail -n +2 $1)
+  done < <(cat $1 | sed '1,/USERID/d')
 }
 
 
